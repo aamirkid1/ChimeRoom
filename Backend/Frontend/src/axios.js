cf5f5fed6dcdd8bf6595364d683f8d78041ca62f
Mohd Aaqib <aamirkid1@gmail.com>
2026-01-05 23:01:40 +0530

    Fix 502: stabilize Socket.IO + server lifecycle

--- Backend/Frontend/src/axios.js
+++ Backend/Frontend/src/axios.js
@@ -1,9 +1,24 @@
-// src/axios.js
+// // src/axios.js
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5002", // your backend origin
+//   withCredentials: true,            //  send JWT cookie on every request
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
+// This checks if the app is running on your computer or on Render
+const BASE_URL = import.meta.env.MODE === "development" 
+  ? "http://localhost:5002" 
+  : "https://chimeroom-chatapp.onrender.com";
+
 const api = axios.create({
-  baseURL: "http://localhost:5002", // your backend origin
-  withCredentials: true,            //  send JWT cookie on every request
+  baseURL: BASE_URL, 
+  withCredentials: true, // send JWT cookie on every request
 });
 
 export default api;
